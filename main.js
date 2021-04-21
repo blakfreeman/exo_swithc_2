a1fa49239d6d22cb9881cc8ddb0f0fc1a9e3c284
blakfreeman <61966557+blakfreeman@users.noreply.github.com>
2021-04-21 09:25:35 +0200

    switch 2

---
+++ main.js
@@ -0,0 +1,19 @@
+let x = "enneigé";
+
+switch (x) {
+    case "pluie":
+        console.log("il pleut je prend un parapluie");
+        break;
+    case "nuageux":
+        console.log("il y a des nuages hahahaha sa mere trop drole");
+        break;
+    case "enneigé":
+        console.log("oussama va se faire victime par la neige");
+        break;
+    case "ensoleilé":
+        console.log("loic va cramer au soleil avec son fusil de chasse");
+        break;
+    default:
+        console.log("introduire un type de météo");
+        break;
+}
